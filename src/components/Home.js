dd5f6f4926cf2ace7c3bcd26eb66f8ebea01dcf9
Alex Lamb <alexlamb@Alexs-MacBook-Pro.local>
2022-07-21 16:38:52 -0600

    Set up basic outline for home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,18 +3,29 @@ import Navbar from './Navbar'
 
 function Home() {
     const [search, setSearch] = useState(false)
+    const [name, setName] = useState("")
 
 
-    if(search){
+    if(!search){
         return (
-            <div>This works</div>
-        )
+          <div>
+            <Navbar />
+            <div className="App">
+                <h1>Search for Users on GitHub!</h1>
+                <form >
+                    <input type="text‘" onChange={(e) => setName(e.target.value)}/>
+                    <button>submit</button>
+                </form>
+            </div>
+          </div>
+        );
     }else{
 
         return (
           <div>
             <Navbar />
-            Search Anything on GitHub!</div>
+            
+            </div>
         )
     }
 }
